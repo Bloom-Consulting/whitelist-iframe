a7588702961990432e7a1835cfa43e9c043e54de
Francisco Pinzón <fpinzn@gmail.com>
2015-12-17 15:28:56 -0500

    Change iframe withelist
    Add kickstarter and thunderclap.

--- assets/javascripts/iframe-whitelist.js
+++ assets/javascripts/iframe-whitelist.js
@@ -1,6 +1,4 @@
 (function() {
-  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.freesound\.org\/embed\/sound\/iframe\/.+/i);
-  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/w\.soundcloud\.com\/player\/.+/i);
-  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/graphics8\.nytimes\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.thunderclap\.it\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.kickstarter\.com\/.+/i);
 })();
-
