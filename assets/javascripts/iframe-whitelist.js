7458f39e4fc255b2cedbf9455cca652ec894fffb
Francisco Pinzón <fpinzn@gmail.com>
2016-01-14 17:14:44 -0500

    Update iframe-whitelist.js
    add sumame and change

--- assets/javascripts/iframe-whitelist.js
+++ assets/javascripts/iframe-whitelist.js
@@ -2,4 +2,6 @@
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.thunderclap\.it\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.kickstarter\.com\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.surveymonkey\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.change\.org\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.sumame\.co\/.+/i);
 })();
